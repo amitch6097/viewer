dd3975017a3cba645567cc49091c4f4345031896
amitch6097 <mitcan@2020.local>
2020-09-27 10:06:58 -0400

    add new favorite apis to front end function endpoints

--- src/services/Functions.ts
+++ src/services/Functions.ts
@@ -3,29 +3,79 @@ import firebase from '../firebase';
 import {
     ICreateBusinessProps,
     ICreateBusinessResponse,
+    ICreateFavoriteGroupProps,
+    ICreateFavoriteGroupResponse,
     ICreateReviewProps,
     ICreateReviewResponse,
+    IGetBusinessesForFavoriteGroupsProps,
+    IGetBusinessesForFavoriteGroupsResponse,
+    IGetFavoriteGroupsProps,
+    IGetFavoriteGroupsResponse,
+    ISetBusinessAsFavoriteProps,
+    ISetBusinessAsFavoriteResponse,
 } from '../../typings/functions';
+
+interface IProps {
+    createBusiness: ICreateBusinessProps;
+    createReview: ICreateReviewProps;
+    createFavoriteGroup: ICreateFavoriteGroupProps;
+    getBusinessesForFavoriteGroup: IGetBusinessesForFavoriteGroupsProps;
+    getFavoriteGroups: IGetFavoriteGroupsProps;
+    setBusinessAsFavorite: ISetBusinessAsFavoriteProps;
+}
+
+interface IResponse {
+    createBusiness: ICreateBusinessResponse;
+    createReview: ICreateReviewResponse;
+    createFavoriteGroup: ICreateFavoriteGroupResponse;
+    getBusinessesForFavoriteGroup: IGetBusinessesForFavoriteGroupsResponse;
+    getFavoriteGroups: IGetFavoriteGroupsResponse;
+    setBusinessAsFavorite: ISetBusinessAsFavoriteResponse;
+}
 
 export class Functions {
     static async createBusiness(
         props: ICreateBusinessProps
     ): Promise<ICreateBusinessResponse> {
-        const createBusiness = firebase
-            .functions()
-            .httpsCallable('createBusiness');
-        const response = await createBusiness(props);
-        return response.data;
+        return await call('createBusiness', props);
     }
 
     static async createReview(
         props: ICreateReviewProps
     ): Promise<ICreateReviewResponse> {
-        const createReview = firebase
-            .functions()
-            .httpsCallable('createReview');
-        const response = await createReview(props);
-        return response.data as ICreateReviewResponse;
+        return await call('createReview', props);
     }
 
+    static async createFavoriteGroup(
+        props: ICreateFavoriteGroupProps
+    ): Promise<ICreateFavoriteGroupResponse> {
+        return await call('createFavoriteGroup', props);
+    }
+
+    static async getBusinessesForFavoriteGroup(
+        props: IGetBusinessesForFavoriteGroupsProps
+    ): Promise<IGetBusinessesForFavoriteGroupsResponse> {
+        return await call('getBusinessesForFavoriteGroup', props);
+    }
+
+    static async getFavoriteGroups(
+        props: IGetFavoriteGroupsProps
+    ): Promise<IGetFavoriteGroupsResponse> {
+        return await call('getFavoriteGroups', props);
+    }
+
+    static async setBusinessAsFavorite(
+        props: ISetBusinessAsFavoriteProps
+    ): Promise<ISetBusinessAsFavoriteResponse> {
+        return await call('setBusinessAsFavorite', props);
+    }
+}
+
+async function call<T extends keyof IProps>(
+    key: T,
+    props: IProps[T]
+): Promise<IResponse[T]> {
+    const func = firebase.functions().httpsCallable(key);
+    const response = await func(props);
+    return response.data as IResponse[T];
 }
