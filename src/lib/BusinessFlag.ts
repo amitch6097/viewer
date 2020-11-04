965561f0944281811aa3e483915755eeb3f22dfb
amitch6097 <mitcan@2020.local>
2020-11-03 20:52:52 -0500

    Add BusinessFlag and BusinessUpdateRequest Classes
    Task #7

---
+++ src/lib/BusinessFlag.ts
@@ -0,0 +1,38 @@
+import { IFlagDocument } from '../../typings/documents';
+
+export interface IBusinessFlag {
+    id: string;
+    businessId: string;
+    type: 'data' | 'inappropriate' | 'closed';
+    text: string;
+    createdBy: string;
+    createdAt?: number;
+}
+
+interface IBusinessFlagData {
+    document: IFlagDocument;
+}
+
+export class BusinessFlag implements IBusinessFlag {
+    constructor(readonly data: IBusinessFlagData) {}
+
+    get id() {
+        return this.data.document.id;
+    }
+    get businessId() {
+        return this.data.document.businessId;
+    }
+    get type() {
+        return this.data.document.type;
+    }
+    get text() {
+        return this.data.document.text;
+    }
+    get createdBy() {
+        return this.data.document.createdBy;
+    }
+    get createdAt() {
+        return this.data.document.createdAt;
+    }
+    
+}
---
+++ src/pages/UpdateBusiness/index.ts
@@ -0,0 +1,3 @@
+export {
+    UpdateBusinessView
+} from './UpdateBusinessView';
